5e90bc3f1420e138b37fe685c47b90e35e866759
Jeferson <jeferson@fetch.ly>
2021-03-18 08:20:41 -0300

    17: added tableview datasource

--- 17 - Flash-Chat-iOS13/Flash Chat iOS13/Controllers/ChatViewController.swift
+++ 17 - Flash-Chat-iOS13/Flash Chat iOS13/Controllers/ChatViewController.swift
@@ -14,10 +14,17 @@ class ChatViewController: UIViewController {
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var messageTextfield: UITextField!
     
+    var messages: [Message] = [
+        Message(sender: "1@2.com", body: "Hey"),
+        Message(sender: "a@b.com", body: "Hello"),
+        Message(sender: "1@2.com", body: "Whats up?")
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         title = Constants.appName
         navigationItem.hidesBackButton = true
+        tableView.dataSource = self
     }
     
     @IBAction func sendPressed(_ sender: UIButton) {
@@ -37,5 +44,20 @@ class ChatViewController: UIViewController {
             self.present(alertController, animated: true, completion: nil)
             return
         }
+    }
+}
+
+//MARK: - TableView Delegate and Data Source
+
+extension ChatViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return messages.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: Constants.cellIdentifier, for: indexPath)
+        cell.textLabel?.text = messages[indexPath.row].body
+        
+        return cell
     }
 }
